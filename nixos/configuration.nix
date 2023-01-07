a0f59494e8ad54d082cbe77e087b2a5a05f5b4d9
Markus328 <markus328@tutanota.com>
2023-01-07 20:28:37 -0300

    fixed nixos configurations

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,82 +9,12 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./filesystems.nix
     ];
 
   #NIX
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
-
-  ##IMPURITIES: filesystem - add/remove/subs according the current hardware
-  #SWAP
-  swapDevices = [{
-    device = "/dev/sda2";
-    priority = 0;
-  }];
-  zramSwap = {
-    enable = true;
-    numDevices = 4;
-    swapDevices = 4;
-    algorithm = "zstd";
-    priority = 32767;
-  };
-
-  #FILESYSTEM
-  fileSystems =
-    let
-      options = [ "rw" "noatime" "ssd" "space_cache=v2" ];
-      compress.options = options ++ [ "compress-force=zstd" ];
-      device = "/dev/sda3";
-      fsType = "btrfs";
-    in
-    {
-      "/" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/nixos/@" ];
-        inherit fsType;
-      };
-      "/userdata" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/@userdata" ];
-        inherit fsType;
-      };
-      "/home" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/nixos/@home" ];
-        inherit fsType;
-      };
-      "/.snapshots" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/snapshots/root/@nixos" ];
-        inherit fsType;
-      };
-      "/home/.snapshots" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/snapshots/@home-nixos" ];
-        inherit fsType;
-      };
-      "/special" = {
-        inherit device;
-        options = options ++ [ "subvol=/@special" ];
-        inherit fsType;
-      };
-      "/userdata/@dotfiles/.snapshots" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/snapshots/@dotfiles" ];
-        inherit fsType;
-      };
-      "/userdata/@workspace/.snapshots" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/snapshots/@workspace" ];
-        inherit fsType;
-      };
-      "/userdata/@games/.snapshots" = {
-        inherit device;
-        options = compress.options ++ [ "subvol=/snapshots/@games" ];
-        inherit fsType;
-      };
-    };
-  ##
 
   ##IMPURITIES: boot - efi only grub
   boot.kernelPackages = pkgs.linuxPackages_zen;
